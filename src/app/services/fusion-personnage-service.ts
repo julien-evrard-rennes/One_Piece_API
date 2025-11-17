bbdb8eff76af7ef67a4e969e388ef8f5c394ab80
julien-evrard-rennes <julien.dominique.evrard@gmail.com>
2025-11-17 16:44:28 +0100

    Fusion-personnage-service complet

--- src/app/services/fusion-personnage-service.ts
+++ src/app/services/fusion-personnage-service.ts
@@ -24,5 +24,27 @@ export class FusionPersonnageService {
         )
     }
 
+    getPersonnageById(id: number): Observable<Personnage> {
+      const mock = this.mockPersoService.getPersonnageById(id);
+      return this.apiPersoService.getPersonnageById(id).pipe(
+        map(api => Personnage.fromApiAndMock(api, mock))
+      );
+    }
+
+    extractAge(age: string | number): number {
+    if (typeof age === "number") return age;
+    if (age === '1 000 ans') return 1000;
+    if (!age) return 0;
+    const match = age.match(/\d+/); 
+    return match ? Number(match[0]) : 0;
+    }
+
+
+    extractPrime(bounty: string): number {
+    if (!bounty) return 0;
+    if (bounty =="inconnu") return 0;
+    const bountyPropre = bounty.split(".").join("");
+    return bountyPropre ? Number(bountyPropre) : 0;
+    }
 
 }
