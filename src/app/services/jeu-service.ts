eb32389e3d0c4c6bf67cd7ee10dd2299485aade7
julien-evrard-rennes <julien.dominique.evrard@gmail.com>
2025-11-24 17:11:01 +0100

    Virer l'implantation (fait freezer le site)

--- src/app/services/jeu-service.ts
+++ src/app/services/jeu-service.ts
@@ -86,7 +86,6 @@ melangerMot(tableauOriginal : string[]) : string[] {
   // clonage du tableau
  const tableauFinal = tableauOriginal;
   // Echanges de position de caractères
-  // while (tableauFinal == tableauOriginal) {
         for (let i = 0; i < tableauFinal.length * 4; i++) {
             let p1 = Math.floor(Math.random() *tableauFinal.length);
             let p2 = Math.floor(Math.random() *tableauFinal.length);
@@ -94,7 +93,6 @@ melangerMot(tableauOriginal : string[]) : string[] {
             tableauFinal[p1] = tableauFinal[p2];
             tableauFinal[p2] = tmp;
         }
-    //}
 
   return tableauFinal ;
 }
