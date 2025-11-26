0ed0a16e01a27a9dc61628e2b1c0fed81ff478ff
julien-evrard-rennes <julien.dominique.evrard@gmail.com>
2025-11-26 14:24:37 +0100

    Rajouts de boutons à la fin

--- src/app/template/jeu-reponse-mot-melange/jeu-reponse-mot-melange.component.ts
+++ src/app/template/jeu-reponse-mot-melange/jeu-reponse-mot-melange.component.ts
@@ -16,6 +16,7 @@ export class JeuReponseMotMelangeComponent implements OnInit  {
   tour:number;
   resultatCadre:string="resultat";
   pourcentage!:number;
+  avisFinal!:string;
 
     constructor(private router: Router) {
     const nav = this.router.getCurrentNavigation();
@@ -28,7 +29,9 @@ export class JeuReponseMotMelangeComponent implements OnInit  {
 
   ngOnInit(): void {
     this.getPourcentage();
-    this.resultatCadre=this.getResultatCadre()
+    this.resultatCadre=this.getResultatCadre();
+    console.log(this.resultatCadre);
+    this.avisFinal=this.getAvisFinal(this.resultatCadre);
   }
 
    getPourcentage() : void {
@@ -48,12 +51,46 @@ export class JeuReponseMotMelangeComponent implements OnInit  {
       else if (this.pourcentage>=25){
         return "resultatBof"
       }
-      else if (this.pourcentage=0){
+      else if (this.pourcentage<25){
         return "resultatPourri"
+      }
+      else if (this.pourcentage==0){
+        return "resultatNul"
       }
       else{
         return "resultatNormal"
       }
     }
+
+      getAvisFinal(resultatCadre: string): string {
+        console.log(resultatCadre);
+        if (this.resultatCadre=="resultat"){
+          return "Bizarre"}
+      if (resultatCadre=="resultatParfait"){
+      return "Bravo ! Tu connais visiblement à la perfection les personnages de One Piece ! \n" +
+      "Ou alors, c'est un énorme coup de pot !"}
+      else if (resultatCadre=="resultatCool"){
+      return "Ok, tu as retrouvé la grande majorité des personnages \n" +
+      "Mais ne te la pète pas trop, c'est peut-être juste la chance !"}
+      else if (resultatCadre=="resultatMoyen"){
+      return "Tu as retrouvé un peu plus de la moitié des personnages. \n" +
+      "Y a vraiment pas de quoi se vanter !"}
+      else if (resultatCadre=="resultatBof"){
+      return "Tu as retrouvé un peu moins de la moitié des personnages. \n" +
+      "J'espère pour toi que tu fera mieux la prochaine fois !"}
+      else if (resultatCadre=="resultatPourri"){
+      return "Et bah dis donc, c'est quoi ce score ? \n" +
+      "Tu connais le manga au moins ?"}
+      else if (resultatCadre=="resultatNul"){
+      return "AUCUNE BONNE RÉPONSE ?\n" + 
+      "Rassure moi, tu en a fait exprès, c'est ça ?  "}
+      else {
+      return "Ce message ne devrait pas s'afficher. Le programme doit avoir un bug."
+      }
+
+
+  }
+
+
 
 }
