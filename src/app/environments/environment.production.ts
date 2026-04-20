65943bab1a5d06b5dccaf03223b76dba547153b6
Julien Evrard <julien.dominique.evrard@gmail.com>
2026-04-20 22:26:01 +0200

    Nouvelle tentative de connexion à la bdd

--- src/app/environments/environment.production.ts
+++ src/app/environments/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     API_PERSO_OP: 'https://api.api-onepiece.com/v2/characters/fr',
     API_GROUPE_OP: 'https://api.api-onepiece.com/v2/crews/fr',
-    DB_PERSO_OP: 'https://one-piece-api.onrender.com/personnages',
-    DB_GROUPE_OP: 'https://one-piece-api.onrender.com/groupes',
+    DB_PERSO_OP: 'https://one-piece-api-w2x9.onrender.com/personnages',
+    DB_GROUPE_OP: 'https://one-piece-api-w2x9.onrender.com/groupes',
 };
 
