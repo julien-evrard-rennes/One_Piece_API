078c50f6d6123ecb3a2f24fb8e34ad78b8c95ed8
Julien Evrard <julien.dominique.evrard@gmail.com>
2026-04-22 14:28:01 +0200

    Ajouts des admin Email et Password !

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   API_GROUPE_OP: 'https://api.api-onepiece.com/v2/crews/fr',
   DB_PERSO_OP: 'http://localhost:3000/personnages',
   DB_GROUPE_OP: 'http://localhost:3000/groupes',
-  ADMIN_EMAIL: 'julien.dominique.evrard@gmail.com',
-  ADMIN_PASSWORD: 'adminDada',
+  ADMIN_EMAIL: '',
+  ADMIN_PASSWORD: '',
 };
